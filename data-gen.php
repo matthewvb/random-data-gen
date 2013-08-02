813c91999623cc6eee5976710779abee519620aa
socrata-matthewvb <matthew.vandenboogart@socrata.com>
2013-08-02 14:01:12 -0400

    initial commit - v 0.1

---
+++ data-gen.php
@@ -0,0 +1,76 @@
+<?php
+// SET THESE VARIABLES
+$colmax = 7; // total number of columns
+$rowmax = 5; // total number of rows
+$download = true; //true: CSV will download | false: on-screen results
+// END SETUP
+
+
+// variables
+$stored_value = "";
+
+
+if ($download){ //download loop
+
+	// set header row
+	for ($i = 1; $i <= $colmax; $i++) {
+
+		$stored_value .= "a" . $i ;
+		if ($i < $colmax) {
+			    $stored_value .=  ", ";
+		}
+	}
+
+	    $stored_value .=  "\n";
+
+	// set data rows
+	for ($i = 1; $i <= $rowmax; $i++) {
+		for ($j = 1; $j <= $colmax; $j++) {
+		        $stored_value .=  rand(1, 100000);
+			if ($j < $colmax) {
+				    $stored_value .=  ", ";
+			}
+			else{
+				    $stored_value .=  "\n";
+			}
+		}
+	}
+
+	// download file
+	header("Content-type: text/csv");
+	header("Content-Disposition: attachment; filename=data.csv");
+	echo $stored_value;
+	
+}
+else{ //on-screen loop
+
+	// set header row
+	for ($i = 1; $i <= $colmax; $i++) {
+	    echo "a" . $i ;
+		if ($i < $colmax) {
+			echo ", ";
+		}
+	}
+
+	echo "<br>";
+
+	// set data rows
+	for ($i = 1; $i <= $rowmax; $i++) {
+		for ($j = 1; $j <= $colmax; $j++) {
+		    echo rand(1, 100000);
+			if ($j < $colmax) {
+				echo ", ";
+			}
+			else{
+				echo "<br>";
+			}
+		}
+	}
+	
+	echo $stored_value;
+	
+}
+
+
+
+?>
